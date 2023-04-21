75e7d3b97f8c7cf88cc584f147cfd42b5b51f76b
anita04021 <131482579+anita04021@users.noreply.github.com>
2023-04-21 20:53:12 +0200

    Add files via upload

---
+++ interaction.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import streamlit as st 
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+st.write("My First Web App")
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+st.write(chart_data)
+
+image = Image.open('images.jpeg')
+st.image(image)
+option = st.selectbox('Select',['a','b','c'])
+st.bar_chart(chart_data[option])
